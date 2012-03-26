d6765d925521050f9c899d8efeb20d48abe3f817
Ondřej Štumpf <ondra.stumpf@seznam.cz>
2012-03-26 20:10:26 +0000

    New project types and structure. VisualLocalizer is official VSPackage, VLTestPackage is only for testing purposes. Successfuly tested context menu items.

---
+++ VisualLocalizer/VLTestPackage/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace OndrejStumpf.VLTestPackage
+{
+    static class PkgCmdIDList
+    {
+        public const uint batchMoveMenuItem = 0x0003;
+        public const uint visualLocalizerTopMenu = 0x0005;
+        public const uint visualLocalizerCodeMenu = 0x0009;
+    };
+}
---
+++ VisualLocalizer/VisualLocalizer/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace OndrejStumpf.VisualLocalizer
+{
+    static class GuidList
+    {
+        public const string guidVisualLocalizerPkgString = "74b8212c-0d66-4fee-99e3-ab64a2d30e50";
+        public const string guidVisualLocalizerCmdSetString = "7b8f6fd3-f366-4fdb-8278-0a3c4395b13e";
+
+        public static readonly Guid guidVisualLocalizerCmdSet = new Guid(guidVisualLocalizerCmdSetString);
+    };
+}
