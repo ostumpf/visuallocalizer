91a92ee5798fdb1245f86634329290f5e822c2b9
Ondřej Štumpf <ondra.stumpf@seznam.cz>
2012-03-27 12:12:12 +0000

    NOOP

---
+++ VisualLocalizer/VLTestingPackage/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace OndrejStumpf.VLTestingPackage
+{
+    static class GuidList
+    {
+        public const string guidVLTestingPackagePkgString = "4d84a08f-4147-4224-8a05-96b47e9d5f6a";
+        public const string guidVLTestingPackageCmdSetString = "051269c8-5bf8-4c5c-8ecd-ccbd3bbe1c65";
+
+        public static readonly Guid guidVLTestingPackageCmdSet = new Guid(guidVLTestingPackageCmdSetString);
+    };
+}
---
+++ VisualLocalizer/VLTestingPackage/PkgCmdID.cs
@@ -0,0 +1,14 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace OndrejStumpf.VLTestingPackage
+{
+    static class PkgCmdIDList
+    {
+        public const uint batchMoveMenuItem = 0x0003;
+        public const uint visualLocalizerTopMenu = 0x0005;
+        public const uint visualLocalizerCodeMenu = 0x0009;
+
+    };
+}
